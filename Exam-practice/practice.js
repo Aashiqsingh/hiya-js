8379430c25861607156cecdef4d5e01b7bb4b262
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-18 16:00:04 +0530

    practice for exam

---
+++ Exam-practice/practice.js
@@ -0,0 +1,77 @@
+function submitHandler(event){
+
+    event.preventDefault();
+
+    // // var pattern = /[A-Za-z0-9]+@$/;
+    // // var pattern = /[6789]{1}[0-9]{9}$/
+
+
+    // var name = document.getElementById("name");
+    // var email = document.getElementById("email");
+    // var mobile = document.getElementById("mobile");
+
+    // var nameErr = document.getElementById("nameErr");
+    // var emailErr = document.getElementById("emailErr");
+    // var mobileErr = document.getElementById("mobileErr");
+
+
+  
+    
+
+
+    // if(mobile.value.match(pattern)){
+    //     mobileErr.innerHTML = "";
+    // }
+    // else{
+    //     mobileErr.innerHTML = "Mobile number must start with 6, 7, or 8 followed by 9 digits";
+    //     mobileErr.style.color = "red";
+    // }
+
+
+
+
+
+
+
+
+    // if(name.value.match(pattern)){
+    //     nameErr.innerHTML = "";
+    // }
+    // else{
+    //     nameErr.innerHTML = "Name must contain only alphabetical characters";
+    //     nameErr.style.color = "red";
+    // }
+
+
+
+
+    // limited character (5 to 15 character)
+
+    // if(name.value.length > 5 && name.value.length < 15){
+    //     nameErr.innerHTML = ""
+    // }
+    // else{
+    //     nameErr.innerHTML = "Name must be between 5 and 15 characters";
+    //     nameErr.style.color = "red";
+    // }
+
+
+
+
+
+
+    // Empty validation
+    // if(name.value === ""){
+    //     nameErr.innerHTML = "Please required name..";
+    //     nameErr.style.color = "red";
+    // }
+    // if(email.value === ""){
+    //     emailErr.innerHTML = "Please required email..";
+    //     emailErr.style.color = "red";
+    // }
+
+    // console.log("Name = ",name.value);
+    // console.log("Email = ",email.value);
+    
+    
+}
